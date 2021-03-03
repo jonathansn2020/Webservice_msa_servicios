07bc63b883419979736fa870e187c88965bb7a41
jonathanksn <jonakevin2018@gmail.com>
2021-03-03 16:52:51 -0500

    Se agrego metodo para realizar busqueda de datos por sede

--- application/models/Api/ApiModel.php
+++ application/models/Api/ApiModel.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 class ApiModel extends CI_Model{
 
@@ -7,19 +7,19 @@ class ApiModel extends CI_Model{
 
     public function ValidarUsuario($codusu){
 
-        $DB = $this->load->database('sec_db', TRUE);             
-        
+        $DB = $this->load->database('sec_db', TRUE);
+
         $DB->select();
         $DB->from('ms_usuarios u');
 
-        $DB->join('ms_rolesh h', 'h.ROL_Codigo_rol = u.ROL_Codigo_rol');       
-                
-        $DB->where("USU_Cod_usuario", $codusu);        
-        
-        $resultado = $DB->get();
-        return $resultado->row();       
+        $DB->join('ms_rolesh h', 'h.ROL_Codigo_rol = u.ROL_Codigo_rol');
 
-    } 
+        $DB->where("USU_Cod_usuario", $codusu);
+
+        $resultado = $DB->get();
+        return $resultado->row();
+
+    }
 
     public function POST_Carga_servicios($data){
 
@@ -33,7 +33,7 @@ class ApiModel extends CI_Model{
         if(isset($idservicio->SRV_Orden_servicio)){
             return $idservicio->SRV_Orden_servicio;
         }
-        
+
     }
 
     public function GET_Nombre_corrido_cliente($codcliente){
@@ -47,5 +47,15 @@ class ApiModel extends CI_Model{
     public function PUT_Carga_servicios($orden, $data){
         $this->db->where($this->table_id, $orden);
         $this->db->update($this->table, $data);
+    }
+
+    public function GET_Sedes($idsede){
+
+        $this->db->select('SD_Tipo_via,SD_Nombre_via,SD_Numero,SD_Interior,SD_Departamento, SD_Provincia, SD_Distrito,SD_Codigo_ubigeo,SD_Responsable');
+        $this->db->from('ms_sedes');
+        $this->db->where('SD_Codigo_sede', $idsede);
+        $resultado = $this->db->get();
+        return $resultado->row();
+
     }
 }
